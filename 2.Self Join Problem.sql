6ff8545b015b2f2eb34644ddb00dbda5eb2751bb
Vishal US <vishalus1997@gmail.com>
2026-01-06 15:05:10 +0530

    2. Self Join Problem Solution

---
+++ 2.Self Join Problem.sql
@@ -0,0 +1,19 @@
+create table emp(emp_id int,emp_name varchar(10),salary int ,manager_id int);
+
+insert into emp values(1,'Ankit',10000,4);
+insert into emp values(2,'Mohit',15000,5);
+insert into emp values(3,'Vikas',10000,4);
+insert into emp values(4,'Rohit',5000,2);
+insert into emp values(5,'Mudit',12000,6);
+insert into emp values(6,'Agam',12000,2);
+insert into emp values(7,'Sanjay',9000,2);
+insert into emp values(8,'Ashish',5000,2);
+
+-- Solution
+
+select e1.emp_id, e1.emp_name, e2.emp_name as manager_name, e1.salary, e2.salary as manager_salary
+from emp e1
+join emp e2
+on e1.manager_id = e2.emp_id
+where e1.salary > e2.salary
+order by e1.emp_id;
